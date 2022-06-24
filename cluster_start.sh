4db5bd46698b8114a25717e13753f4a4ce4979fd
AdityaKoranga <nishakornga@gmail.com>
2022-06-24 10:23:42 +0530

    changed the position of swapoff

--- cluster_start.sh
+++ cluster_start.sh
@@ -4,15 +4,15 @@ set -xe
 
 #THIS IS FOR SINGLE NODE KUBEADM CLUSTER
 
+#SWAPOFF
+sudo swapoff -a
+
 #initialisation
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16
 
 mkdir -p $HOME/.kube
   yes | sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
   sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-# SWAPOFF
-sudo swapoff -a
 
 #flannel
 kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
